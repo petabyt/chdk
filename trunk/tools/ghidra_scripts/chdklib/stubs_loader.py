petabyt/chdk

commit a384e20168996d731229db16b8153790dacbade0

ghidra scripts - update stubs loader to handle makefile style comments
git-svn-id: http://subversion.assembla.com/svn/chdk@5447 6794e30b-3f2a-0410-a806-a2bbca1c07ff

trunk/tools/ghidra_scripts/chdklib/stubs_loader.py
@@ -92,7 +92,7 @@ class StubsFileParser:
         # everything else in section is ignored for now
 
     def process_makefile_val(self,body):
-        m = re.match('([^\s]+)\s*=\s*([^\s]+)(?:\s+(.*))?',body)
+        m = re.match('([^\s]+)\s*=\s*([^\s#]+)(?:#)?(?:\s+(.*))?',body)
         # should all be formated like makefile lines
         if not m:
             self.section=None
@@ -436,26 +436,18 @@ class StubsData:
             elif ar['name'] == 'RAM code':
                 if ar['start_adr'] < smisc['max_ram_addr']:
                     smisc['ar_ramcode'] = ar
-                else: # older finsig_thumb2 version didn't label TCM
+                else:
                     smisc['ar_btcmcode'] = ar
-            elif ar['name'] == 'TCM code':
-                smisc['ar_btcmcode'] = ar
-            elif ar['name'] == 'EVEC':
-                smisc['ar_evec'] = ar
 
         if not 'adr_ranges_extra' in smisc:
             smisc['adr_ranges_extra'] = []
 
         if 'CAM_UNCACHED_BIT' in smisc['camera_h_vals']:
             ucadr = int(smisc['camera_h_vals']['CAM_UNCACHED_BIT']['val'],16)
-        elif smisc['os_info']['os'] == 'VXWORKS':
-            # all known vx cams use this, not detected by sig finder
-            ucadr = 0x10000000
+            if 'max_ram_addr' in smisc:
+                self.add_guess_adr_range('uncached',ucadr,smisc['max_ram_addr']+1)
         else:
             ucadr = False
-
-        if 'max_ram_addr' in smisc and ucadr:
-            self.add_guess_adr_range('uncached',ucadr,smisc['max_ram_addr']+1)
 
         # main MMIO for digic < 6, d6 and d7 still have some there
         self.add_guess_adr_range('mmio',0xC0000000,0x01000000)
@@ -466,7 +458,7 @@ class StubsData:
                 self.add_guess_adr_range('dtcm',0x80000000,0x1000)
             elif ucadr == 0x10000000:
                 self.add_guess_adr_range('dtcm',0x40000000,0x1000)
-            elif ucadr:
+            else:
                 self.warn('unexpected uncached adr 0x%08x'%(ucadr))
         else:
             # 2M starting at 0xdfe00000 is a different region (Omar TCM), d7 have TCM? in 0xdff

commit 0705b37dff02f3975eb7403f2f78767c466ebce6

ghidra scripts - fix some additional bugs introduced in r5447 (not actually r5377 mentioned in previous commit)
git-svn-id: http://subversion.assembla.com/svn/chdk@5503 6794e30b-3f2a-0410-a806-a2bbca1c07ff

trunk/tools/ghidra_scripts/chdklib/stubs_loader.py
@@ -451,19 +451,28 @@ class StubsData:
             elif ar['name'] == 'RAM data':
                 smisc['ar_ramdata'] = ar
             elif ar['name'] == 'RAM code':
-                smisc['ar_ramcode'] = ar
+                if ar['start_adr'] < smisc['max_ram_addr']:
+                    smisc['ar_ramcode'] = ar
+                else: # older finsig_thumb2 version didn't label TCM
+                    smisc['ar_btcmcode'] = ar
             elif ar['name'] == 'TCM code':
                 smisc['ar_btcmcode'] = ar
+            elif ar['name'] == 'EVEC':
+                smisc['ar_evec'] = ar
 
         if not 'adr_ranges_extra' in smisc:
             smisc['adr_ranges_extra'] = []
 
         if 'CAM_UNCACHED_BIT' in smisc['camera_h_vals']:
             ucadr = int(smisc['camera_h_vals']['CAM_UNCACHED_BIT']['val'],16)
-            if 'max_ram_addr' in smisc:
-                self.add_guess_adr_range('uncached',ucadr,smisc['max_ram_addr']+1)
+        elif smisc['os_info']['os'] == 'VXWORKS':
+            # all known vx cams use this, not detected by VX sig finder
+            ucadr = 0x10000000
         else:
             ucadr = False
+
+        if 'max_ram_addr' in smisc and ucadr:
+            self.add_guess_adr_range('uncached',ucadr,smisc['max_ram_addr']+1)
 
         # main MMIO for digic < 6, d6 and d7 still have some there
         self.add_guess_adr_range('mmio',0xC0000000,0x01000000)
@@ -474,7 +483,7 @@ class StubsData:
                 self.add_guess_adr_range('dtcm',0x80000000,0x1000)
             elif ucadr == 0x10000000:
                 self.add_guess_adr_range('dtcm',0x40000000,0x1000)
-            else:
+            elif ucadr:
                 self.warn('unexpected uncached adr 0x%08x'%(ucadr))
         else:
             # 2M starting at 0xdfe00000 is a different region (Omar TCM), d7 have TCM? in 0xdff